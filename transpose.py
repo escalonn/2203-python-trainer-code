5dca1c8baf64c9dae77de8542a3172a72d42550d
Nicholas Escalona <nicholas.escalona@gmail.com>
2022-03-22 13:21:04 -0500

    implement transpose a few ways

--- transpose.py
+++ transpose.py
@@ -1,7 +1,25 @@
+
+uppercase = [char.upper() for line in line_list if line != ""
+                          for char in line.strip()]
+# equiv to:
+for line in line_list:
+    if line != "":
+        for c in line.strip():
+            c.upper()
+
+#                           1. -------------------------------->
+#              2. ---------> |
+stripped_list = [line.strip() for line in line_list if line != ""]
+# equiv to:
+result = []
+for line in line_list:
+    if line != "":
+        result.append(line.strip())
+
 def transpose(lines):
     input_list = lines.split('\n')  # or splitlines
     input_height = len(input_list)
-    input_width = get_input_width(input_list)
+    input_width = max(len(x) for x in input_list)
 
     output_list = []
 
@@ -15,7 +33,44 @@ def transpose(lines):
     return '\n'.join(output_list)
 
 
-my_lines = ['Hello', 'World']
+# def get_input_width(input_list):
+#     # generator expression - doesn't use memory to store a whole list we don't need
+#     return max(len(x) for x in input_list)
+
+    # python has "comprehensions"
+    #   a way to build a new collection based on some other collection(s)
+    # lengths = [len(x) for x in input_list]
+    # lengths = map(len, input_list)
+    # return max(lengths)
+
+    # return max(map(len, input_list))
+    # return len(max(input_list, key=len))
+    # max_length = 0
+    # for row in input_list:
+    #     if len(row) > max_length:
+    #         max_length = len(row)
+    # return max_length
+    # max_length = 0
+    # for i in range(len(input_list)):
+    #     row = input_list[i]
+    #     if len(row) > max_length:
+    #         max_length = len(row)
+    # return max_length
+
+
+def get_char(input_list, rownum, colnum):
+    # try:
+    #     return input_list[rownum][colnum]
+    # except IndexError:
+    #     return '*'
+    if rownum < len(input_list) and colnum < len(input_list[rownum]):
+        return input_list[rownum][colnum]
+    return '*'
+
+
+# my_lines = ['Hello', 'World']
+my_lines = ['A', 'BC']
+# my_lines = []
 input_str = '\n'.join(my_lines)
 print(transpose(input_str))
 print(repr(transpose(input_str)))
